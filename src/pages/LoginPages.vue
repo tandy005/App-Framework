3d74ac6fbaed3f69218423dc49ff5959bca145aa
gvjavx <naufalhilmi1809@gmail.com>
2024-01-09 13:51:28 +0700

    login page

--- src/pages/LoginPages.vue
+++ src/pages/LoginPages.vue
@@ -1,24 +1,76 @@
 <template>
-  <div class="q-pa-md">
-    <div class="row">
-      <div class="col">
-        .col
-      </div>
-      <div class="col">
-        <h1>Login</h1>
-        <form @submit.prevent="login">
-          <input type="text" v-model="username" placeholder="Username" />
-          <input type="password" v-model="password" placeholder="Password" />
-          <button type="submit">Login</button>
-        </form>
-      </div>
-    </div>
+  <q-layout class="login-bg-img">
+    <div class="wrap justify-center items-start content-center login-card">
+      <q-card
+        class="my-card text-white section-login"
+      >
+        <q-card-section class="box-login">
+          <div class="row">
+            <div class="col-6 image-box ">
+              <q-img
+                src="../assets/login-banner.png"
+                spinner-color="white"
+                class="q-img-login-banner"
+              />
+              <p style="width: 85%;">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque voluptatem totam, architecto cupiditate officia rerum, error dignissimos praesentium libero ab nemo.
+              </p>
+            </div>
+            <div class="col-6 " >
+              <q-card 
+                class="my-card text-white q-card-login"
+              >
+                <q-card-sections>
+                  <q-img 
+                    src="../assets/logo-icon.svg"
+                    spinner-color="white"
+                    class="q-img-login-logo"
+                  />
+                  <q-form
+                    @submit="submit"
+                    class="q-gutter-md q-form-login"
+                  >
+                    <q-input
+                      filled
+                      v-model="username"
+                      label="Username"
+                      lazy-rules
+                      :rules="[ val => val && val.length > 0 || 'Please type something']"
+                    />
+
+                    <q-input
+                      filled
+                      type="password"
+                      v-model="password"
+                      label="Password"
+                      lazy-rules
+                      :rules="[ val => val && val.length > 0 || 'Please type something']"
+                    />
+                    <div>
+                      <q-btn label="Login" type="submit" color="primary" class="q-btn-login"/>
+                    </div>
+                  </q-form>
+                </q-card-sections>
+              </q-card>
+             
+            </div>
+          </div>
+        </q-card-section>
+      </q-card>
     
-  </div>
+    </div>
+  </q-layout>
  
 </template>
 <script>
 export default {
+  // setup (){
+  //   const urlBanner = ref("../assets/login-banner.png")
+
+  //   return {
+  //     urlBanner,
+  //   }
+  // },
   data() {
     return {
       username: "",
